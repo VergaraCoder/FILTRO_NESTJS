a8d480277ae77161f4fe3c42597ac1de67c8b775
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-10-07 14:35:21 -0500

    feat: creation of user and role modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { TournamentsModule } from './tournaments/tournaments.module';
 import { TournamentsPlayersModule } from './tournaments-players/tournaments-players.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { UsersModule } from './users/users.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [PlayersModule, TournamentsModule, TournamentsPlayersModule, AuthModule, CommonModule],
+  imports: [PlayersModule, TournamentsModule, TournamentsPlayersModule, AuthModule, CommonModule, UsersModule, RoleModule],
   controllers: [],
   providers: [],
 })
