53dbd0824cec8504fa4f1b5173757113ac73271b
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-10-07 14:32:26 -0500

    feat: creation of modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PlayersModule } from './players/players.module';
+import { TournamentsModule } from './tournaments/tournaments.module';
+import { TournamentsPlayersModule } from './tournaments-players/tournaments-players.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PlayersModule, TournamentsModule, TournamentsPlayersModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookie from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config=app.get(ConfigService);
+  app.use(cookie(config.get("SIGNED_COOKIE")));
   await app.listen(3000);
 }
 bootstrap();
