c6804033fb0f501deb04f180244c4effe1f9b3c0
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-10-07 14:33:51 -0500

    feat: creation od common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PlayersModule } from './players/players.module';
 import { TournamentsModule } from './tournaments/tournaments.module';
 import { TournamentsPlayersModule } from './tournaments-players/tournaments-players.module';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [PlayersModule, TournamentsModule, TournamentsPlayersModule, AuthModule],
+  imports: [PlayersModule, TournamentsModule, TournamentsPlayersModule, AuthModule, CommonModule],
   controllers: [],
   providers: [],
 })
