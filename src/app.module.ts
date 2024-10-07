472dcec14cfbd372317e0429a22cdad77ba0f34b
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-10-07 14:51:55 -0500

    feat: creation of connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,23 @@ import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
 import { RoleModule } from './role/role.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmCredentials } from './common/database/dbconfig/db.config';
 
 @Module({
-  imports: [PlayersModule, TournamentsModule, TournamentsPlayersModule, AuthModule, CommonModule, UsersModule, RoleModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:".env"
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useClass:TypeOrmCredentials
+    }),
+    PlayersModule, TournamentsModule, TournamentsPlayersModule, AuthModule, CommonModule, UsersModule, RoleModule],
   controllers: [],
   providers: [],
 })
